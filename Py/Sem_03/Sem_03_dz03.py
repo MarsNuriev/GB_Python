158bde0c1b1d6b6639ca9b8e79947ca3a4ae3e04
Mars <nuriev.mars@gmail.com>
2022-10-03 15:07:28 +0300

    задача sem_03_dz03 решена

---
+++ Py/Sem_03/Sem_03_dz03.py
@@ -0,0 +1,22 @@
+# Задайте список из вещественных чисел.
+# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import random
+arr = []
+arr_div = []
+n = random.randint(1, 10)
+for arr_index in range(n):
+    arr.append(round(random.uniform(0, 10), 2))
+
+
+for arr_index1 in range(0,len(arr)):
+    arr_div.append(round(arr[arr_index1] % 1,2))
+
+Razn_max_min = int(round((max(arr_div) - min(arr_div))*100,0))
+
+
+print(f'количество элементов в списке = {n}')
+print(f'исходный список: {arr} -> разница между макс и мин остатком равна: {Razn_max_min}')
+
