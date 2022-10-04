1c6e10986b057a85bbedc99311892d9ebfd4d9f3
Mars <nuriev.mars@gmail.com>
2022-10-04 16:31:54 +0300

    задача sem_01_dz03 добавил защиту от неверного ввода

--- Py/Sem_01/Sem_01_dz03.py
+++ Py/Sem_01/Sem_01_dz03.py
@@ -4,16 +4,21 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+while True:
+    try:
+        x =int(input('введите x: '))
+        y =int(input('введите y: '))
+        if x != 0:
+            if y !=0:
+                break
+    except ValueError:
+        print('--------')
 
-x =int(input('введите x: '))
-y =int(input('введите y: '))
 if x > 0 and y > 0:
     print(f'(x = {x}, y = {y}) -> 1')
 elif x > 0 and y < 0:
     print(f'(x = {x}, y = {y}) -> 2')
 elif x < 0 and y < 0:
     print(f'(x = {x}, y = {y}) -> 3')
-elif x < 0 and y > 0:
-    print(f'(x = {x}, y = {y}) -> 4')
 else:
-    print('введите значение х иил y не равное 0') 
+    print(f'(x = {x}, y = {y}) -> 4')
