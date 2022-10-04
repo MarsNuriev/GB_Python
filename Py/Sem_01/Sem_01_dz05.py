b9b8766aa581fe0adb2cba4ff6e5c445accc0aae
Mars <nuriev.mars@gmail.com>
2022-10-04 17:01:53 +0300

    задача sem_01_dz05 добавил защиту от неверного ввода

--- Py/Sem_01/Sem_01_dz05.py
+++ Py/Sem_01/Sem_01_dz05.py
@@ -2,10 +2,14 @@
 # Пример:
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
-
-p_ax = int(input('введите коордиату x точки A: '))
-p_ay = int(input('введите коордиату y точки A: '))
-p_bx = int(input('введите коордиату x точки B: '))
-p_by = int(input('введите коордиату y точки B: '))
-dlina = ((p_ax-p_bx)**2 + (p_ay-p_by)**2)**(0.5)
+while True:
+    try:
+        p_ax = int(input('введите коордиату x точки A: '))
+        p_ay = int(input('введите коордиату y точки A: '))
+        p_bx = int(input('введите коордиату x точки B: '))
+        p_by = int(input('введите коордиату y точки B: '))
+        break
+    except ValueError:
+        print('введите число!')    
+dlina = round(((p_ax-p_bx)**2 + (p_ay-p_by)**2)**(0.5),2)
 print(f'A ({p_ax}, {p_ay}); B ({p_bx}, {p_by}) -> {dlina}')
