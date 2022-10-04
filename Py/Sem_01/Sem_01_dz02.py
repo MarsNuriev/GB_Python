9a2c1dd6170f8fdd8f24acaa545aa471e7e04bd2
Mars <nuriev.mars@gmail.com>
2022-10-04 16:20:39 +0300

    задача sem_01_dz02 исправил решение для всех значений предикат

--- Py/Sem_01/Sem_01_dz02.py
+++ Py/Sem_01/Sem_01_dz02.py
@@ -1,8 +1,10 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. 
 # ¬ - не, ⋁ - или, ⋀ - и
 
-x = input('введите значение x: ')
-y = input('введите значение y: ')
-z = input('введите значение z: ')
-
-print(not(x or y or z) == (not(x) and not(y) and not(z)))
+for x in range(0,2):
+    for y in range (0,2):
+        for z in range (0,2):
+            if (not(x or y or z) == (not(x) and not(y) and not(z))):
+                print(f'x = {x}, y = {y}, z = {z} -> выражение истинно')
+            else:
+                 print(f'x = {x}, y = {y}, z = {z} -> выражение ложно')
