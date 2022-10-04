4409fe473ed77fadb2a54313935d2e3d01ea598c
Mars <nuriev.mars@gmail.com>
2022-10-04 16:38:40 +0300

    задача sem_01_dz04 добавил защиту от неверного ввода

--- Py/Sem_01/Sem_01_dz04.py
+++ Py/Sem_01/Sem_01_dz04.py
@@ -1,11 +1,13 @@
 #Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-
-from re import X
-
-
-max_x = int(input('введите максимальную x: '))
-max_y = int(input('введите максимальную y: '))
-quart = int(input('выберите четверть от 1 до 4: '))
+while True:
+    try:
+        max_x = int(input('введите максимальную x: '))
+        max_y = int(input('введите максимальную y: '))
+        quart = int(input('выберите четверть от 1 до 4: '))
+        if quart in (1,4):
+            break
+    except ValueError:
+        print('введите число!')    
 if quart == 1:
     print(f'координаты x = от 0 до {max_x}, y = от 0 до {max_y}')
 elif quart == 2:
