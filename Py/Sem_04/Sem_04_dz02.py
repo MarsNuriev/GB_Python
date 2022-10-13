b626622436e387ff00abde5f5c3b52b8b22a32aa
Mars <nuriev.mars@gmail.com>
2022-10-13 14:41:04 +0300

    задача sem_04_dz02 решена

---
+++ Py/Sem_04/Sem_04_dz02.py
@@ -0,0 +1,26 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N
+from multiprocessing import Value
+
+
+while True:
+    try:
+        n = int(input('Введите N: '))
+        break
+    except ValueError:
+        print('введите целое число: ')
+
+
+def mnozh(value):
+    value_print = value
+    my_list = []
+    mn = 2
+    while value >= mn:
+        if value % mn == 0:
+            my_list.append(mn)
+            value = value / mn
+            mn = 2
+        else:
+            mn += 1
+    return print (f'n = {value_print} -> {my_list}')
+
+mnozh(n)
