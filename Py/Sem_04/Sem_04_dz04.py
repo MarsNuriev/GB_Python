98c3a228bdc54a09cae1de7e2ad3e52f80bd8c5a
Mars <nuriev.mars@gmail.com>
2022-10-14 15:55:50 +0300

    задача sem_04_dz04 решена

--- Py/Sem_04/Sem_04_dz04.py
+++ Py/Sem_04/Sem_04_dz04.py
@@ -1,5 +1,32 @@
-# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена 
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена
 # и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+# import random
+# while True:
+#     try:
+#         k = int(input('Введите k: '))
+#         break
+#     except ValueError:
+#         print('введите целое число: ')
 
+
+from random import randint
+k = int(input('Введите натуральную степень k:'))
+def urav(value):
+    my_list = []
+    my_list1 = []
+    for i in range(value+1):
+        my_list.append(randint(0, 100))
+    # print(my_list)
+    for koef in range(value+1)[::-1]:
+        my_list1.append(f'{my_list[koef]}x^{koef}')
+    my_list1 = ' + '.join(map(str,my_list1))
+    my_list1 = my_list1.replace('x^0','')
+    my_list1 = my_list1.replace('x^1','x')
+    my_list1 = f'{my_list1} = 0'
+    return my_list1
+# urav(k)
+f = open('uravnenie.txt','w')
+f.write(urav(k))
+f.close
