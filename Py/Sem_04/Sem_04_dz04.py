fc538c8fe0e487a8a63c36e7aa99ed93abfa0a3b
Mars <nuriev.mars@gmail.com>
2022-10-14 16:05:03 +0300

    Sem_04_dz04 добавил проверку на 0

--- Py/Sem_04/Sem_04_dz04.py
+++ Py/Sem_04/Sem_04_dz04.py
@@ -20,13 +20,14 @@ def urav(value):
         my_list.append(randint(0, 100))
     # print(my_list)
     for koef in range(value+1)[::-1]:
-        my_list1.append(f'{my_list[koef]}x^{koef}')
+        if my_list[koef]!=0:
+            my_list1.append(f'{my_list[koef]}x^{koef}')
     my_list1 = ' + '.join(map(str,my_list1))
     my_list1 = my_list1.replace('x^0','')
     my_list1 = my_list1.replace('x^1','x')
     my_list1 = f'{my_list1} = 0'
     return my_list1
-# urav(k)
+# print(urav(k))
 f = open('uravnenie.txt','w')
 f.write(urav(k))
 f.close
